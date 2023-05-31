ec87ac949c8603309e554e07176ac9e9299e6072
mrvldo <mariorivaldo86@gmail.com>
2023-05-31 11:29:51 +0700

    commit

--- app.js
+++ app.js
@@ -1,39 +1,41 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-require('dotenv').config()
+require("dotenv").config();
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}))
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 // import Route
-const authRoutes = require('./routes/auth')
-const postRoutes = require('./routes/post')
-app.use('/auth', authRoutes)
-app.use('/post', postRoutes)
+const authRoutes = require("./routes/auth");
+const postRoutes = require("./routes/post");
+app.use("/auth", authRoutes);
+app.use("/post", postRoutes);
 
-mongoose.connect(process.env.DATABASE_CONNECTION, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(process.env.DB_CONNECTION, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-let db = mongoose.connection
+let db = mongoose.connection;
 
 // Handle Error
-db.on('error', console.error.bind(console, 'Error Establishing a Database'))
+db.on("error", console.error.bind(console, "Error Establishing a Database"));
 
 // Handle Succes
-db.once('open', () => {
-    console.log("Database is connected")
-})
+db.once("open", () => {
+  console.log("Database is connected");
+});
+
 app.listen(process.env.PORT, () => {
-    console.log(Example app listening on port ${process.env.PORT})
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
